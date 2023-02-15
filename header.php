87cc1e31429a3ab2091d7e46f0b3f95e27590eee
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-14 21:19:23 -0300

    Inclusão das fontes.

--- header.php
+++ header.php
@@ -21,6 +21,9 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="https://gmpg.org/xfn/11">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pacifico&family=Poppins&display=swap">
 <?php wp_head(); ?>
 </head>
 
