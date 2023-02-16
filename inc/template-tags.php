4aa8a732c4dc206fa55bcb64e8bdf0fa460b225f
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-16 20:44:39 -0300

    WIP.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -56,9 +56,10 @@ endif;
 
 if ( ! function_exists( 'larissa_post_author' ) ) :
     function larissa_post_author() {
-        $author_string = '<span class="post-author" title="Autora"><a href="#">%1$s</a></span>';
+        $author_string = '<span class="post-author" title="Autora"><a href="%1$s">%2$s</a></span>';
         $author_string = sprintf(
             $author_string,
+            the_author_posts_link(),
             get_the_author()
         );
         return sprintf(
