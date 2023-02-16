1185e85651181c388161f8fd2ab12d00b1de0b82
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-16 19:51:36 -0300

    WIP.

--- index.php
+++ index.php
@@ -19,36 +19,35 @@
 get_header();
 ?>
 
-<div class="wrap">
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main">
-            <?php
-            if ( have_posts() ) :
-                // Start the Loop.
-                while ( have_posts() ) :
-                    the_post();
-                    /*
-                     * Include the Post-Format-specific template for the content.
-                     * If you want to override this in a child theme, then include a file
-                     * called content-___.php (where ___ is the Post Format name) and that
-                     * will be used instead.
-                     */
-                    get_template_part( 'template-parts/post/content', get_post_format() );
-                endwhile;
-                the_posts_pagination(
-                    array(
-                        'prev_text'          => larissa_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'larissa' ) . '</span>',
-                        'next_text'          => '<span class="screen-reader-text">' . __( 'Next page', 'larissa' ) . '</span>' . larissa_get_svg( array( 'icon' => 'arrow-right' ) ),
-                        'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'larissa' ) . ' </span>',
-                    )
-                );
-            else :
-                get_template_part( 'template-parts/post/content', 'none' );
-            endif;
-            ?>
-        </main><!-- #main -->
-    </div><!-- #primary -->
-    <?php get_sidebar(); ?>
-</div><!-- .wrap -->
+<div class="row g-0">
+    <div class="col-12 col-lg-9">
+        <?php
+        if ( have_posts() ) :
+            // Start the Loop.
+            while ( have_posts() ) :
+                the_post();
+                /*
+                    * Include the Post-Format-specific template for the content.
+                    * If you want to override this in a child theme, then include a file
+                    * called content-___.php (where ___ is the Post Format name) and that
+                    * will be used instead.
+                    */
+                get_template_part( 'template-parts/post/content', get_post_format() );
+            endwhile;
+            the_posts_pagination(
+                array(
+                    'prev_text'          => larissa_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'larissa' ) . '</span>',
+                    'next_text'          => '<span class="screen-reader-text">' . __( 'Next page', 'larissa' ) . '</span>' . larissa_get_svg( array( 'icon' => 'arrow-right' ) ),
+                    'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'larissa' ) . ' </span>',
+                )
+            );
+        else :
+            get_template_part( 'template-parts/post/content', 'none' );
+        endif;
+        ?>
+    </div>
+    <div class="col-12 col-lg-3 ps-4">
+        <?php get_sidebar(); ?>
+    </div>
 <?php
 get_footer();
