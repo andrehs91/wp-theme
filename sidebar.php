9fd963033a6b957569fba3290a582ffb4f573723
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-18 19:53:47 -0300

    WIP.

--- sidebar.php
+++ sidebar.php
@@ -11,8 +11,8 @@
  * @version 1.0
  */
 
-if ( is_active_sidebar( 'menu-lateral' ) ) {
+if ( ! is_active_sidebar( 'menu-lateral' ) ) {
+    return;
+} else {
     dynamic_sidebar( 'menu-lateral' );
-} else { ?>
-    <div class="alert alert-secondary" role="alert">O menu lateral foi definido.</div>
-<?php }
+}
