1a749993d1e01225c3854ec4f2f20c271bc43dbc
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-18 21:02:30 -0300

    WIP.

--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -14,20 +14,14 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-        <?php larissa_edit_link( get_the_ID() ); ?>
-    </header><!-- .entry-header -->
-    <div class="entry-content">
-        <?php
-            the_content();
-
-            wp_link_pages(
-                array(
-                    'before' => '<div class="page-links">' . __( 'Pages:', 'larissa' ),
-                    'after'  => '</div>',
-                )
-            );
-            ?>
-    </div><!-- .entry-content -->
+    <?php larissa_edit_link( get_the_ID() ); ?>
+    <?php
+        the_content();
+        wp_link_pages(
+            array(
+                'before' => '<div class="page-links">' . __( 'Pages:', 'larissa' ),
+                'after'  => '</div>',
+            )
+        );
+    ?>
 </article><!-- #post-<?php the_ID(); ?> -->
