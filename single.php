1ea0f29e4facc6e608176f3153ecf3ed3043b306
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-19 17:04:36 -0300

    WIP.

--- single.php
+++ single.php
@@ -14,26 +14,27 @@
 get_header();
 ?>
 
-<main class="col-12 col-lg-9">
-    <div class="alert alert-primary" role="alert">single.php</div>
-    <div id="primary" class="content-area">
-        <?php
-        // Start the Loop.
-        while ( have_posts() ) :
-            the_post();
-            get_template_part( 'template-parts/post/content', get_post_format() );
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;
-            the_post_navigation(
-                array(
-                    'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'larissa' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'larissa' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . larissa_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
-                    'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'larissa' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'larissa' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . larissa_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-                )
-            );
-        endwhile; // End the loop.
-        ?>
+<main class="col-12 col-lg-9 content">
+    <div class="mb-3 mb-md-4">
+        <?php if ( is_active_sidebar( 'anuncio-superior' ) ) dynamic_sidebar( 'anuncio-superior' ); ?>
+    </div>
+    <?php
+    while ( have_posts() ) :
+        the_post();
+        get_template_part( 'template-parts/post/content', get_post_format() );
+        if ( comments_open() || get_comments_number() ) :
+            comments_template();
+        endif;
+        the_post_navigation(
+            array(
+                'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'larissa' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'larissa' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . larissa_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
+                'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'larissa' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'larissa' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . larissa_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
+            )
+        );
+    endwhile;
+    ?>
+    <div class="mt-3 mt-md-4">
+        <?php if ( is_active_sidebar( 'anuncio-inferior' ) ) dynamic_sidebar( 'anuncio-inferior' ); ?>
     </div>
 </main>
 <aside class="col-12 col-lg-3 ps-lg-4">
