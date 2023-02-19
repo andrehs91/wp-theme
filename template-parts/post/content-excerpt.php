f00b668ade35c8d1ae4b07c949fbeec733d03651
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-19 15:23:20 -0300

    WIP.

--- template-parts/post/content-excerpt.php
+++ template-parts/post/content-excerpt.php
@@ -17,20 +17,22 @@
 
 <article id="post-<?php the_ID(); ?>" class="post-excerpt">
     <?php if ( 'post' === get_post_type() ) : ?>
-        <div class="post-category">
-            <div class="alert alert-primary m-0 px-3 py-2 d-inline-block">
-                <?php
-                    $categories = get_the_category();
-                    foreach ($categories as $category) {
-                        $category_link = get_category_link($category);
-                        echo '<a href="' . esc_url($category_link) . '" title="Categoria ' . esc_attr($category->name) . '">' . esc_html($category->name) . '</a>';
-                    }
-                ?>
+        <div class="d-flex justify-content-between">
+            <div class="post-author-date">
+                <?= larissa_post_author(); ?>
+                <?= larissa_post_date(); ?>
             </div>
-        </div>
-        <div class="post-author-date">
-            <?= larissa_post_author(); ?>
-            <?= larissa_post_date(); ?>
+            <div class="post-category">
+                <div class="alert alert-primary m-0 px-3 py-2 d-inline-block">
+                    <?php
+                        $categories = get_the_category();
+                        foreach ($categories as $category) {
+                            $category_link = get_category_link($category);
+                            echo '<a href="' . esc_url($category_link) . '" title="Categoria ' . esc_attr($category->name) . '">' . esc_html($category->name) . '</a>';
+                        }
+                    ?>
+                </div>
+            </div>
         </div>
     <?php endif; ?>
     <?php the_title( sprintf( '<h2 class="post-title" title="Título da publicação"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
@@ -39,12 +41,12 @@
         <?php the_excerpt(); ?>
     </div>
     <?php if ( 'post' === get_post_type() ) : ?>
-        <div class="post-tags" title="Marcações">
+        <div class="post-tags" title="Tags">
             <?php
                 $tags = get_the_tags();
                 foreach ($tags as $tag) {
                     $tag_link = get_the_tags($tag);
-                    echo '<a href="' . esc_url($tag_link) . '" title="Marcação ' . esc_attr($tag->name) . '">#' . esc_html($tag->name) . '</a>';
+                    echo '<a href="' . esc_url($tag_link) . '" title="Tag ' . esc_attr($tag->name) . '">#' . esc_html($tag->name) . '</a>';
                 }
             ?>
         </div>
