f1666c7e735308853c512ff7aaf3915dac91ce8d
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-19 16:34:52 -0300

    WIP.

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@
 get_header();
 ?>
 
-<main class="col-12 col-lg-9">
+<main class="col-12 col-lg-9 pb-0 pb-lg-4">
     <div class="alert alert-primary" role="alert">404.php</div>
     <div class="alert alert-primary m-0" role="alert">Parece que nada foi encontrado neste local. Que tal fazer uma pesquisa?</div>
 </main>
--- search.php
+++ search.php
@@ -14,13 +14,15 @@
 get_header();
 ?>
 
-<main class="col-12 col-lg-9">
+<main class="col-12 col-lg-9 pb-0 pb-lg-4">
     <div class="alert alert-primary" role="alert">search.php</div>
-    <div class="mb-3 mb-md-4">
-        <?php if ( is_active_sidebar( 'anuncio-superior' ) ) dynamic_sidebar( 'anuncio-superior' ); ?>
-    </div>
     <?php
     if ( have_posts() ) :
+    ?>
+        <div class="mb-3 mb-md-4">
+            <?php if ( is_active_sidebar( 'anuncio-superior' ) ) dynamic_sidebar( 'anuncio-superior' ); ?>
+        </div>
+    <?php
         while ( have_posts() ) :
             the_post();
             get_template_part( 'template-parts/post/content', 'excerpt' );
@@ -32,13 +34,15 @@ get_header();
                 'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'larissa' ) . ' </span>',
             )
         );
+    ?>
+        <div class="mt-3 mt-md-4">
+            <?php if ( is_active_sidebar( 'anuncio-inferior' ) ) dynamic_sidebar( 'anuncio-inferior' ); ?>
+        </div>
+    <?php
     else :
         ?><div class="alert alert-primary m-0" role="alert">Desculpe, mas nada corresponde aos teus termos de pesquisa. Por favor, tente novamente com algumas palavras-chave diferentes.</div><?php
     endif;
     ?>
-    <div class="mt-3 mt-md-4">
-        <?php if ( is_active_sidebar( 'anuncio-inferior' ) ) dynamic_sidebar( 'anuncio-inferior' ); ?>
-    </div>
 </main>
 <aside class="col-12 col-lg-3 ps-lg-4">
     <?php get_sidebar(); ?>
