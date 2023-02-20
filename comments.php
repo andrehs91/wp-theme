c2ee0aa8ab474a98d453cb04e5dae8bddd5378ea
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-20 15:43:30 -0300

    WIP.

--- comments.php
+++ comments.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The template for displaying comments
  *
@@ -9,27 +8,50 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package WordPress
- * @subpackage Larissa
- * @since Larissa 1.0
+ * @subpackage Twenty_Seventeen
+ * @since Twenty Seventeen 1.0
  * @version 1.0
  */
 
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
 if ( post_password_required() ) {
     return;
 }
 ?>
 
 <div id="comments" class="comments-area">
+
     <?php
+    // You can start editing here -- including this comment!
     if ( have_comments() ) :
         ?>
-        <h3 class="comments-title title-decoration">
-            <?php if ( absint( get_comments_number() > 1 ) ) {
-                echo absint( get_comments_number() ) . ' Comentários';
+        <h2 class="comments-title">
+            <?php
+            $comments_number = get_comments_number();
+            if ( '1' === $comments_number ) {
+                /* translators: %s: Post title. */
+                printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'twentyseventeen' ), get_the_title() );
             } else {
-                echo absint( get_comments_number() ) . ' Comentário';
-            } ?>
-        </h3>
+                printf(
+                    /* translators: 1: Number of comments, 2: Post title. */
+                    _nx(
+                        '%1$s Reply to &ldquo;%2$s&rdquo;',
+                        '%1$s Replies to &ldquo;%2$s&rdquo;',
+                        $comments_number,
+                        'comments title',
+                        'twentyseventeen'
+                    ),
+                    number_format_i18n( $comments_number ),
+                    get_the_title()
+                );
+            }
+            ?>
+        </h2>
+
         <ol class="comment-list">
             <?php
                 wp_list_comments(
@@ -42,19 +64,26 @@ if ( post_password_required() ) {
                 );
             ?>
         </ol>
+
         <?php
         the_comments_pagination(
             array(
-                'prev_text' => '«<span class="screen-reader-text">Anterior</span>',
-                'next_text' => '»<span class="screen-reader-text">Próximo</span>'
+                'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous', 'twentyseventeen' ) . '</span>',
+                'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
             )
         );
-    endif;
+
+    endif; // Check for have_comments().
+
+    // If comments are closed and there are comments, let's leave a little note, shall we?
     if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
         ?>
-        <p class="no-comments">Os comentários estão fechados.</p>
+
+        <p class="no-comments"><?php _e( 'Comments are closed.', 'twentyseventeen' ); ?></p>
         <?php
     endif;
+
     comment_form();
     ?>
+
 </div><!-- #comments -->
