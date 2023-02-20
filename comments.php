7a9140b9532364c5ffd6e2609785ed8c6def2bf2
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-20 15:51:46 -0300

    WIP.

--- comments.php
+++ comments.php
@@ -20,34 +20,16 @@ if ( post_password_required() ) {
 ?>
 
 <div id="comments" class="comments-area">
-
     <?php
-    // You can start editing here -- including this comment!
     if ( have_comments() ) :
         ?>
-        <h2 class="comments-title">
-            <?php
-            $comments_number = get_comments_number();
-            if ( '1' === $comments_number ) {
-                /* translators: %s: Post title. */
-                printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'larissa' ), get_the_title() );
+        <h3 class="comments-title title-decoration">
+            <?php if ( absint( get_comments_number() > 1 ) ) {
+                echo absint( get_comments_number() ) . ' Comentários';
             } else {
-                printf(
-                    /* translators: 1: Number of comments, 2: Post title. */
-                    _nx(
-                        '%1$s Reply to &ldquo;%2$s&rdquo;',
-                        '%1$s Replies to &ldquo;%2$s&rdquo;',
-                        $comments_number,
-                        'comments title',
-                        'larissa'
-                    ),
-                    number_format_i18n( $comments_number ),
-                    get_the_title()
-                );
-            }
-            ?>
-        </h2>
-
+                echo absint( get_comments_number() ) . ' Comentário';
+            } ?>
+        </h3>
         <ol class="comment-list">
             <?php
                 wp_list_comments(
@@ -55,31 +37,24 @@ if ( post_password_required() ) {
                         'avatar_size' => 100,
                         'style'       => 'ol',
                         'short_ping'  => true,
-                        'reply_text'  => larissa_get_svg( array( 'icon' => 'mail-reply' ) ) . __( 'Reply', 'larissa' ),
+                        'reply_text'  => 'Responder',
                     )
                 );
             ?>
         </ol>
-
         <?php
         the_comments_pagination(
             array(
-                'prev_text' => larissa_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous', 'larissa' ) . '</span>',
-                'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'larissa' ) . '</span>' . larissa_get_svg( array( 'icon' => 'arrow-right' ) ),
+                'prev_text' => '«<span class="screen-reader-text">Anterior</span>',
+                'next_text' => '»<span class="screen-reader-text">Próximo</span>'
             )
         );
-
-    endif; // Check for have_comments().
-
-    // If comments are closed and there are comments, let's leave a little note, shall we?
+    endif;
     if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
         ?>
-
-        <p class="no-comments"><?php _e( 'Comments are closed.', 'larissa' ); ?></p>
+        <p class="no-comments">Os comentários estão fechados.</p>
         <?php
     endif;
-
     comment_form();
     ?>
-
 </div><!-- #comments -->
