b9edc91ec349e2d61958c48b7f57e891e524f875
Andre Henrique dos Santos <andrehs@outlook.com.br>
2023-02-20 16:05:12 -0300

    WIP.

--- comments.php
+++ comments.php
@@ -14,13 +14,17 @@
  * @version 1.0
  */
 
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
 if ( post_password_required() ) {
     return;
 }
 ?>
 
 <div id="comments" class="comments-area">
-
     <?php
     // You can start editing here -- including this comment!
     if ( have_comments() ) :
@@ -47,7 +51,6 @@ if ( post_password_required() ) {
             }
             ?>
         </h2>
-
         <ol class="comment-list">
             <?php
                 wp_list_comments(
@@ -60,26 +63,20 @@ if ( post_password_required() ) {
                 );
             ?>
         </ol>
-
         <?php
         the_comments_pagination(
             array(
-                'prev_text' => larissa_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous', 'larissa' ) . '</span>',
-                'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'larissa' ) . '</span>' . larissa_get_svg( array( 'icon' => 'arrow-right' ) ),
+                'prev_text' => '«<span class="screen-reader-text">Anterior</span>',
+                'next_text' => '»<span class="screen-reader-text">Próximo</span>'
             )
         );
-
     endif; // Check for have_comments().
-
     // If comments are closed and there are comments, let's leave a little note, shall we?
     if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
         ?>
-
         <p class="no-comments"><?php _e( 'Comments are closed.', 'larissa' ); ?></p>
         <?php
     endif;
-
     comment_form();
     ?>
-
 </div><!-- #comments -->
